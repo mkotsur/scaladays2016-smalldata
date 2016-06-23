a7659b98b10dbe345a75ff88471ca72cdf3a4c59
Mike Kotsur <miccots@gmail.com>
2016-06-23 16:01:53 +0200

    Init small data

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "scaladays2016-smalldata"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies += "com.github.pathikrit" %% "better-files" % "2.16.0"
---
+++ src/main/scala/scala/amsterdam/SmallData.scala
@@ -0,0 +1,19 @@
+package scala.amsterdam
+
+import better.files._
+
+object SmallData extends App {
+
+
+  val tagsFile = resource"/tags.txt"
+  val tags = tagsFile.getLines().toList
+    .map(_.split(":::"))
+    .flatMap {
+      case Array(topic, tagline) =>  tagline.split(",\\ ?")
+    }
+    .groupBy(identity)
+    .mapValues(_.size)
+    .toList.sortBy(_._2  * -1)
+
+  println(tags)
+}
